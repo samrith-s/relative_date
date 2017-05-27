0e00bee16e1a37904f1cc3ab618ef5b2d98b945f
Samrith Shankar <samrith@ithaka.travel>
2017-05-28 00:53:11 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# relative_date
-A tiny NPM package which, when given a date, returns a human-friendly relative value from the current date.
+# Relative Date
---
+++ relative_date.js
@@ -0,0 +1,33 @@
+
+var moment = require('moment');
+
+module.exports = function(date) {
+    date = moment(date);
+    let parsedDate = {
+        value:date.toISOString(),
+        text:null,
+        valid: true
+    };
+
+    if(!date._isValid) {
+        parsedDate.valid = false;
+        parsedDate.text = "Invalid Date";
+        return parsedDate;
+    }
+
+    let diff = moment(Date.now()).diff(date, 'days');
+
+
+    if (diff < 0)
+        parsedDate.text = moment(date).fromNow(true) + " from now";
+    else if (diff < 1)
+      parsedDate.text = date.format("h:mma");
+    else if (diff === 1)
+      parsedDate.text = "Yesterday";
+    else if (diff > 1 && diff < 7)
+      parsedDate.text = date.format("dddd");
+    else
+      parsedDate.text = date.format("M/D/YYYY");
+
+    return parsedDate;
+}
