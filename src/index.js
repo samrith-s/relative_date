bc5c0b461d00f9001ca4f9ef3100760813b912b1
Samrith Shankar <samrith.s@media.net>
2018-08-22 06:06:24 +0530

    Updated module to format correctly and minor perf improvements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export default 'Welcome to relative_date'
+const moment = require('moment');
+
+module.exports = function(date) {
+  date = moment(date);
+  let parsedDate = {
+    value: date.toISOString(),
+    text: null,
+    valid: true
+  };
+
+  if (!date._isValid) {
+    parsedDate.valid = false;
+    parsedDate.text = 'Invalid Date';
+    return parsedDate;
+  }
+
+  let diff = moment(Date.now()).diff(date, 'days');
+
+  if (diff < 0) {
+    parsedDate.text = moment(date).fromNow(true) + ' from now';
+  } else if (diff < 1) {
+    parsedDate.text = date.format('h:mma');
+  } else if (diff === 1) {
+    parsedDate.text = 'Yesterday';
+  } else if (diff > 1 && diff < 7) {
+    parsedDate.text = date.format('dddd');
+  } else {
+    parsedDate.text = date.format('D MMMM, YYYY');
+  }
+
+  return parsedDate;
+};
